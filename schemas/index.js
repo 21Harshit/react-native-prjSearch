116139380d9c5dd37bdc19a4e23ac9261df7beb8
Harshit Kaushik <harshitkaushik432@gmail.com>
2024-01-16 00:42:24 +0530

    On branch main  Changes to be committed: 	modified:   schemas/index.js 	new file:   schemas/post.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,31 @@
-export const schemaTypes = []
+
+export const schemaTypes = [ {name: "posts",
+title: "Posts",
+type: "document",
+fields:[{
+   name: "image",
+   title:"Image",
+   type:"image"
+},
+{
+   name:  "title",
+   title: "Title",
+   type: "string",
+   validation: (Rule) => Rule.required(),
+},
+{
+   name:  "hashtag",
+   title: "Hastag",
+   type: "string",
+   validation: (Rule) => Rule.required(),
+},
+{
+   name:  "body",
+   title: "Body",
+   type: "string" 
+}
+
+]
+}
+]
+
---
+++ schemas/post.js
@@ -0,0 +1,27 @@
+// export default{
+//     name: "post",
+//     title: "posts",
+//     type: "document",
+//     fields:[{
+//        name: "image",
+//        title:"Image",
+//        type:"image"
+//    },
+//    {
+//        name:  "title",
+//        title: "Title",
+//        type: "string" 
+//    },
+//    {
+//        name:  "hashtag",
+//        title: "Hastag",
+//        type: "string" 
+//    },
+//    {
+//        name:  "post",
+//        title: "Body",
+//        type: "string" 
+//    }
+   
+//     ]
+//    }
